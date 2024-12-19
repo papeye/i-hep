64e15aafe0b7c9a75f11f676c840a30602cb85b4
Paweł Pajewski <pawelpajewski@Pawes-Mac-mini.lan>
2024-12-19 11:03:17 +0100

    Implement tamplate tab view

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,14 +1,45 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends HookWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Hello World!'),
+    final tabController = useTabController(initialLength: 2);
+
+    return Scaffold(
+      appBar: AppBar(
+        bottom: TabBar(
+          controller: tabController,
+          tabs: [
+            for (final tab in HomeScreenTab.values)
+              Tab(icon: Icon(tab.iconData)),
+          ],
+        ),
+      ),
+      body: TabBarView(
+        controller: tabController,
+        children: [
+          for (final tab in HomeScreenTab.values)
+            Center(child: Icon(tab.iconData)),
+        ],
       ),
     );
   }
+}
+
+enum HomeScreenTab {
+  dashboard,
+  list;
+
+  IconData get iconData => switch (this) {
+        dashboard => Icons.dashboard,
+        list => Icons.list,
+      };
+
+  String get pathSegment => switch (this) {
+        dashboard => 'dashboard',
+        list => 'list',
+      };
 }
